40447ce119b8cd92a2ddf622ea09b90312d7f6e1
WIN-U72O24JDSN4\user <valentyna.pronina@nure.ua>
2021-04-16 17:28:05 +0300

    Balancing

--- ConsoleApp1/BalancedBinarySearchTree.cs
+++ ConsoleApp1/BalancedBinarySearchTree.cs
@@ -10,6 +10,7 @@ namespace Lab1
         class Node
         {
             public int data;
+            public int height;
             public Node left, right;
 
             public Node(int d)
@@ -69,7 +70,57 @@ namespace Lab1
             // Normal insert
             root = recursiveAdd(root, item);
             
-            // TODO rebalance
+            // Balancing
+            root = rebalance(root);
+        }
+        
+        private int height(Node node) {
+            return node == null ? -1 : node.height;
+        }
+        
+        private void updateHeight(Node current) {
+            current.height = 1 + Math.Max(height(current.left), height(current.right));
+        }
+        
+        private int getBalance(Node node) {
+            return node == null ? 0 : height(node.right) - height(node.left);
+        }
+        
+        private Node rotateLeft(Node y) {
+            Node x = y.right;
+            Node z = x.left;
+            x.left = y;
+            y.right = z;
+            updateHeight(y);
+            updateHeight(x);
+            return x;
+        }
+
+        private Node rotateRight(Node y) {
+            Node x = y.left;
+            Node z = x.left;
+            x.right = y;
+            y.left = z;
+            updateHeight(y);
+            updateHeight(x);
+            return x;
+        }
+
+        private Node rebalance(Node z) {
+            updateHeight(z);
+            int balance = getBalance(z);
+            if (balance > 1) {
+                if (height(z.right.right) <= height(z.right.left)) {
+                    z.right = rotateRight(z.right);
+                }
+                z = rotateLeft(z);
+            } else if (balance < -1) {
+                if (height(z.left.left) < height(z.left.right)) {
+                    z.left = rotateLeft(z.left);
+                }
+                z = rotateRight(z);
+            }
+            return z;
         }
     }
 }
